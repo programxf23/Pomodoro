ecdc13e83e8ea4d15c33ae01d7fee3d3e7921f95
Fernando <fh1273509@gmail.com>
2024-02-26 18:04:51 -0300

    le damos formato al tiempo

--- main.js
+++ main.js
@@ -38,24 +38,53 @@ function renderTasks() {
   const html = tasks.map((task) => {
     return `
       <div class = "task">
-      <div class = "completed">${task.completed ? `<span class = "done">Done</span>` : `<button class = "start-button" data-id = "${task.id}">Start</button>`}</div>
+      <div class = "completed">${
+        task.completed
+          ? `<span class = "done">Done</span>`
+          : `<button class = "start-button" data-id = "${task.id}">Start</button>`
+      }</div>
       <div class = "title">${task.title}</div>
       
       </div>    
     `;
   });
-  const tasksContainer = document.querySelector('#tasks');
-  tasksContainer.innerHTML = html.join('');
+  const tasksContainer = document.querySelector("#tasks");
+  tasksContainer.innerHTML = html.join("");
 
-  const startButton = document.querySelectorAll('.tasks .startButton');
+  const startButton = document.querySelectorAll(".tasks .startButton");
   startButton.forEach((button) => {
-    button.addEventListener('click', (e) => {
-      if(!timer){
-        const id = button.getAttribute('data-id');     
-
-
+    button.addEventListener("click", (e) => {
+      if (!timer) {
+        const id = button.getAttribute("data-id");
+        startButtonHandler(id);
+        button.textContent = "In progress..";
       }
     });
-
   });
+}
+
+function startButtonHandler(id) {
+  time = 25 * 60;
+  current = id;
+  const taskIndex = tasks.findIndex((task) => task.id === id);
+  const taskName = documentq.querySelector("#time #taskName");
+  taskName.textContent = tasks[taskIndex].title;
+
+  timer.setInterval(() => {
+    //funcion indefinida, hasta que yo la detenga. En cambio setTimeout se ejecuta despues de un periodo de tiempo
+    timeHandler(id);
+  }, 1000);
+}
+function timeHandler(id) {
+time--;
+renderTime();
+}
+
+function renderTime() {//me permite darle funcion a un numero
+  const timeDiv = document.querySelector('#time #value');
+  const minutes = parseInt(time / 60);
+  const seconds = parseInt(time % 60);
+
+  //le damos el formato:
+  timeDiv.textContent = `${minutes < 10 ? '0' : ''}${minutes}:${seconds < 10 ? '0' : ''}${seconds}`;
 }
